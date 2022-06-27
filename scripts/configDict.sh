2ffa28697d42b1e3c66253619619e7e87b305416
Gordon Crone <g.crone@ucl.ac.uk>
2022-06-27 11:47:02 +0100

    Simplify scrript for starting server in pocket

--- scripts/configDict.sh
+++ scripts/configDict.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 cd /dunedaq/run/
-source dbt-env.sh
-#dbt-workarea-env
-dbt-setup-release dunedaq-v2.11.1-cs8
 source dbt-pyvenv/bin/activate
-PYLIB=$(ls -l dbt-pyvenv/bin/python|sed -e "s=.* \(.*\)/bin/python=\1/lib=")
-export LD_LIBRARY_PATH=${PYLIB}:${LD_LIBRARY_PATH}
-export configMap=configtest
 export FLASK_APP=install/configclient/bin/configDict
 python -m flask run --host=0.0.0.0
